d7adda90994bad4ee1c95cc60b8b4147513d0173
ImeshiTaVi <167306689+ImeshiTaVi@users.noreply.github.com>
2025-05-23 17:23:27 +0530

    Update ngo_donation_page.dart
    create NgoDonationPage StatefulWidget structure

--- lib/screens/ngo_donation_page.dart
+++ lib/screens/ngo_donation_page.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+class NgoDonationPage extends StatefulWidget {
+  const NgoDonationPage({super.key});
+
+  @override
+  State<NgoDonationPage> createState() => _NgoDonationPageState();
+}
 
