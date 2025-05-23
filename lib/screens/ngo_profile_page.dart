329ee8d918b3db882945d762a1ed524556aecb0f
IsharaMuthuminiKA <166861472+IsharaMuthuminiKA@users.noreply.github.com>
2025-05-23 19:29:48 +0530

    Update ngo_profile_page.dart
    This section imports necessary Flutter and project-specific screens, and defines the NgoProfilePage as a stateful widget which manages the NGO profile interface with editable and dynamic content.

--- lib/screens/ngo_profile_page.dart
+++ lib/screens/ngo_profile_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+import 'ngo_home_screen.dart';
+import 'ngo_donation_map_screen.dart';
+import 'ngo_notification_page.dart';
+import 'ngo_profile_page.dart';
+
+class NgoProfilePage extends StatefulWidget {
+  const NgoProfilePage({super.key});
+
+  @override
+  _NgoProfilePageState createState() => _NgoProfilePageState();
+}
 
