eedc2d7b8fe2c3e5e08abf5206ef3b16de96a198
IsharaMuthuminiKA <166861472+IsharaMuthuminiKA@users.noreply.github.com>
2025-05-24 13:07:14 +0530

    Update ngo_profile_page.dart
    This section declares state variables used to control editing mode, bottom navigation selection, and text input fields. It also initializes the NGO’s profile image path.

--- lib/screens/ngo_profile_page.dart
+++ lib/screens/ngo_profile_page.dart
@@ -1,1 +1,11 @@
+class _NgoProfilePageState extends State<NgoProfilePage> {
+  bool _isEditing = false;
+  int _selectedIndex = 3; //Profile tab
+
+  late TextEditingController _nameController;
+  late TextEditingController _emailController;
+  late TextEditingController _phoneController;
+  late TextEditingController _passwordController;
+
+  String ngoLogoAsset = 'assets/ngo_logo.png';
 
