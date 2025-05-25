9ca4953aee895a7b80aec8e0b448cf7bff017f6f
Tainynavoda <141064945+Tainynavoda@users.noreply.github.com>
2025-05-25 08:52:21 +0530

    Update donar_notification_screen.dart
    Set up NotificationScreen layout with appbar and back navigation. This includes the red-colored appBar, title, and a back button for navigation.

--- lib/screens/donar_notification_screen.dart
+++ lib/screens/donar_notification_screen.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Notification'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
 
