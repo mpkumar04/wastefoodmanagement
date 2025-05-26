67d8f86c07e12b9c321231dfde1eacb6f5fc5b97
BoyagodaRGAD <166887983+BoyagodaRGAD@users.noreply.github.com>
2025-05-26 21:10:42 +0530

    Create donar_profile_screen.dart
    Created a StatefulWidget named DonorProfileScreen.
    Added base structure with a corresponding state class (_DonorProfilePageState).
    Prepared the widget to manage donor profile information and UI states.

--- lib/screens/donar_profile_screen.dart
+++ lib/screens/donar_profile_screen.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
 
+class DonorProfileScreen extends StatefulWidget {
+const DonorProfileScreen({super.key});
+
+@override
+_DonorProfilePageState createState() => _DonorProfilePageState();
+}
