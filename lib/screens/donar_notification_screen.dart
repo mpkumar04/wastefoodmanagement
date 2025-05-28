cd51ee5a987579ddc7feb54b50ceefbedb2ea809
Tainynavoda <141064945+Tainynavoda@users.noreply.github.com>
2025-05-28 10:54:34 +0530

    Update donar_notification_screen.dart
    Added body section to notificationscreen using listview and padding. Included sample notifications using reusable card widget method.

--- lib/screens/donar_notification_screen.dart
+++ lib/screens/donar_notification_screen.dart
@@ -14,4 +14,22 @@ class NotificationScreen extends StatelessWidget {
           },
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            _buildNotificationCard(
+              "Donation has been sent to Social Project",
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc imperdiet ornare aliquet enim.",
+              "5 Apr 2024",
+            ),
+            _buildNotificationCard(
+              "Donation has been sent to Social Project",
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc imperdiet ornare aliquet enim.",
+              "3 Apr 2024",
+            ),
+            // Add more notifications if necessary
+          ],
+        ),
+      ),
 
