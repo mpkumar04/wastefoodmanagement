b86f6fda9025b345918a4f8bbf654c973a387e34
BoyagodaRGAD <166887983+BoyagodaRGAD@users.noreply.github.com>
2025-05-28 22:03:56 +0530

    Initialize profile controllers and lifecycle methods
    Declared TextEditingControllers for name, email, phone and password.
    Implemented initState to set default  values for the profile fields.
    Added dispose method to clean up controllers and prevent memory leaks.

--- lib/screens/donar_profile_screen.dart
+++ lib/screens/donar_profile_screen.dart
@@ -6,3 +6,31 @@ const DonorProfileScreen({super.key});
 @override
 _DonorProfilePageState createState() => _DonorProfilePageState();
 }
+class _DonorProfilePageState extends State<DonorProfileScreen> {
+bool _isEditing = false;
+
+late TextEditingController _nameController;
+late TextEditingController _emailController;
+late TextEditingController _phoneController;
+late TextEditingController _passwordController;
+
+String donorLogoAsset = 'assets/images/donor_profile.jpg';
+
+@override
+void initState() {
+super.initState();
+_nameController = TextEditingController(text: 'Donor name');
+_emailController = TextEditingController(text: 'donor@example.com');
+_phoneController = TextEditingController(text: '+1234567890');
+_passwordController = TextEditingController();
+}
+
+@override
+void dispose() {
+_nameController.dispose();
+_emailController.dispose();
+_phoneController.dispose();
+_passwordController.dispose();
+super.dispose();
+}
+
